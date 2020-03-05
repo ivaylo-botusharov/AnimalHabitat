852ad3acc23b01a004ef6a4cd8914b3c78df8173
ivaylo.botusharov <ivaylo.botusharov@proficredit.bg>
2020-03-05 09:39:46 +0200

    Added Database diagram

--- Ecology/Ecology.Client.CreativeTim/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ Ecology/Ecology.Client.CreativeTim/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,20 +5,16 @@ import { CreateDistributionComponent } from '../../create-distribution/create-di
 import { SpeciesDistributionComponent } from '../../species-distribution/species-distribution.component';
 
 export const AdminLayoutRoutes: Routes = [
-    // {
-    //   path: '',
-    //   children: [ {
-    //     path: 'dashboard',
-    //     component: HomeComponent
-    // }]},
-    // {
-    //     path: '',
-    //     children: [ {
-    //         path: 'upgrade',
-    //         component: UpgradeComponent
-    //     }]
-    // }
-    { path: 'home',           component: HomeComponent },
-    { path: 'create-distribution',   component: CreateDistributionComponent },
-    { path: 'species-distribution',     component: SpeciesDistributionComponent },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'create-distribution',
+        component: CreateDistributionComponent
+    },
+    {
+        path: 'species-distribution',
+        component: SpeciesDistributionComponent
+    },
 ];
